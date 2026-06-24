114fe7891d023cea75f2c9043ca0035a6601e539
asjat <asjatbagwan@gmail.com>
2026-06-24 11:23:01 +0530

    selected element in js

---
+++ script.js
@@ -0,0 +1,5 @@
+const count = document.querySelector(".count")
+const minus = document.querySelector(".minus-btn")
+const plus = document.querySelector(".plus-btn")
+const input = document.querySelector(".input1")
+const reset = document.querySelector(".reset-btn")
