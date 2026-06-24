e6e13ca2169528ad2e2ff3083cedfbab0df37996
asjat <asjatbagwan@gmail.com>
2026-06-24 11:34:32 +0530

    sleceted element in js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const minus = document.querySelector(".minus-btn")
 const plus = document.querySelector(".plus-btn")
 const input = document.querySelector(".input1")
 const reset = document.querySelector(".reset-btn")
+
+minus.addEventListener("click",()=>{
+   count.innertext=9 
+})
